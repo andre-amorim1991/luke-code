79aa953a6678cb1b11e6783714e126a41d5c91af
andre-amorim1991 <72902054+andre-amorim1991@users.noreply.github.com>
2020-12-17 18:11:15 -0300

    criação das sections 2 e 3

--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 
 import './styles.css';
-import './stylo.css';
+// import './stylo.css';
 
 import separator from '../../assets/images/separator.svg';
 import yoda from '../../assets/images/characters/yoda.png';
+
+import CourseReactRedux from '../../assets/images/courses/react-redux.jpg';
+import CoursePhp from '../../assets/images/courses/php.jpg';
+import CourseFlutter from '../../assets/images/courses/flutter.jpg';
+import CardCourse from '../../componentes/CardCouse';
 
 const Home = () => {
     return (
@@ -38,6 +43,51 @@ const Home = () => {
                             <img src={yoda} alt="Mestre Yoda com sabre de luz" />
                         </section>
                     </main>
+                </div>
+            </div>
+            <div className="section-2">
+                <div className="container">
+                    <h2>+3 Cursos Completos</h2>
+                    <p>
+                        Na Luke code, você tem acesso a 3 cursos gratuitos, basta se cadastrar na plataforma.
+                    </p>
+                    <section className="card-container">
+                        <CardCourse image={CourseReactRedux} description="Desenvolvimento front-end React e Redux">
+                            Desenvolvimento front-end <br /> React e Redux
+                        </CardCourse>
+
+                        <CardCourse image={CoursePhp} description="PHP 7 - Trabalhando com PSRs">
+                            PHP 7 - Trabalhando com PSRs
+                        </CardCourse>
+                        
+                        
+                        <CardCourse image={CourseFlutter} description="Desenvolvimento IOS e Android com flutter">
+                            Desenvolvimento IOS e Android <br/> com flutter
+                        </CardCourse>
+                        
+                    </section>
+                    <button>
+                        Inscreva-se
+                    </button>
+                </div>
+            </div>
+
+            <div className="section-3">
+                <div className="container">
+                    <h2>Oque oeferecemos para você</h2>
+                    <section className="categories-container">
+
+                        <div className="category-card">
+                            <div className="category-card-image">
+                               
+                            </div>
+                            <div class="category-card-content">
+                                 <h4>BACK-END</h4>
+                                <p>22 Cursos</p>
+                            </div>
+
+                        </div>
+                    </section>
                 </div>
             </div>
         </>
